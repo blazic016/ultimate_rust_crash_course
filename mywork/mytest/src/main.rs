0d91108a1bc03bb639b4ee7687d47f7c1a27f5ea
Blazic <Miroslav.Blazic@rt-rk.com>
2024-02-18 20:06:48 +0100

    threads

--- mywork/mytest/src/main.rs
+++ mywork/mytest/src/main.rs
@@ -1,9 +1,21 @@
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let num = 5;
+    // Kreiranje nove niti i pokretanje funkcije u toj niti
+    let handle = thread::spawn(|| {
+        for i in 1..=5 {
+            println!("Thread: {}", i);
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
 
-    // Iteracija kroz opseg brojeva od 0 do num - 1
-    for i in 0..num {
-        println!("{} Hello", i);
+    // Glavna nit nastavlja sa radom
+    for i in 1..=3 {
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1000));
     }
+
+    // Čekanje da se nit završi (često se koristi kako bi se sačekalo da se sve niti završe pre nego što se program završi)
+    handle.join().unwrap();
 }
